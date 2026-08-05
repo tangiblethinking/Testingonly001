48771bdfdfe659972efc83699a85e51947831658
tangiblethinking <80935836+tangiblethinking@users.noreply.github.com>
2026-08-04 21:42:47 -0700

    Update text in Home component
    Removed 'Partner.' from the text list in Home component.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -221,7 +221,7 @@ export default function Home({ setPage }) {
           >Christopher Kenreigh</motion.p>
 
           {[
-            { text: 'Strategy. Design. Builder. Partner.',             color: INK,      delay: 0.45 },
+            { text: 'Strategy. Design. Builder.',             color: INK,      delay: 0.45 },
             { text: 'Excellence',               color: BLUE,     delay: 0.55, italic: true },
             { text: 'for digital products',       color: INK,  delay: 0.63 },
             { text: 'and spaces.',              color: INK,  delay: 0.70 },
